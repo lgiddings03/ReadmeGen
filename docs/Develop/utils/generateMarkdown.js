d44785f462fff450f5b01dd66c55d7f10c2e437c
Liz Giddings <lgiddings03@gmail.com>
2021-02-13 13:15:52 -0600

    finally figured out the correct syntax

--- docs/Develop/utils/generateMarkdown.js
+++ docs/Develop/utils/generateMarkdown.js
@@ -1,40 +1,119 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  await licenseBadge();
-
-  async function licenseBadge() {
-    await octokit.request('GET /licenses');
+  if(license === "none") {
+    return "";
+  }
+  else {
+    license = license.replace(/\s/g , "_");
+    return `![License](https://img.shields.io/static/v1?label=License&message=${license}&color=blue)`;
   }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  renderLink();//img.shields.io/github/license/lgiddings03/ReadmeGen?style=for-the-badge)
-
-
-    function renderLink() {
-      ![GitHub](https);
-    }
+  if(license === "none") {
+    return "";
+  }
+  else {
+    license = license.replace(/\s/g , "_");
+    return `![License](https://img.shields.io/static/v1?label=License&message=${license}&color=blue)`;
+  }
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  await octokit.request('GET /repos/{lgiddings03}/{ReadmeGen}/license', {
-    owner: 'Liz Giddings',
-    repo: 'ReadmeGen'
-  })
+  const licenseArray = [
+    {
+      name: "GNU AGPLv3",
+      link: "https://www.gnu.org/licenses/agpl-3.0"
+    },
+    {
+      name: "GNU GPLv3",
+      link: "https://www.gnu.org/licenses/gpl-3.0"
+    },
+    {
+      name: "GNU LGPLv3",
+      link: "https://www.gnu.org/licenses/lgpl-3.0"
+    },
+    {
+      name: "Mozilla Public License 2.0",
+      link: "https://opensource.org/licenses/MPL-2.0"
+    },
+    {
+      name: "Apache License 2.0",
+      link: "https://opensource.org/licenses/Apache-2.0"
+    },
+    {
+      name: "MIT License",
+      link: "https://opensource.org/licenses/MIT"
+    },
+    {
+      name: "Boost Software License 1.0",
+      link: "https://www.boost.org/LICENSE_1_0.txt"
+    },
+    {
+      name: "The UNLicense",
+      link: "http://unlicense.org/"
+    }
+  ]
+
+  if(license === "none") {
+    return "";
+  }
+  else {
+
+    let licenseLink = licenseArray.find(e => e.name === license);
+
+    return licenseLink.link;
+
+  };
+
 }
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(_markdown) {
-  var generateMarkdown = require('markdown-it')({
-    html: true,
-    linkify: true,
-    typographer: true
-  });
+function generateMarkdown(data) {
+  let licenseBadge = renderLicenseBadge(data.license);
+  let licenseSection = renderLicenseSection(data.license);
 
-  module.exports = generateMarkdown;
+  return `
+  # ${data.title}
+  ${licenseBadge}
+  ## Table of Contents: \n
+  [Description](#description)\n
+  [Installation](#installation)\n
+  [Usage](#usage)\n
+  [Contributing](#contributing)\n
+  [Tests](#tests)\n
+  [License](#license)\n
+  [Questions](#questions)\n
+  <hr>\n
+  ## Description: \n
+  ${data.description} \n
+  <hr>\n
+  ## Installation: \n
+  ${data.installationInstructions}\n
+  <hr>\n
+  ## Usage: \n
+  ${data.usageInformation}\n
+  <hr>\n
+  ## Contributing: \n
+  ${data.contributionGuidelines}\n
+  <hr>\n
+  ## Tests: \n
+  ${data.testInstructions}\n
+  <hr>\n
+  ### License: \n
+  ${licenseSection}\n
+  <hr>\n
+  ### Questions: \n
+  If you would like to learn more, please take a look through my github below:\n
+  <a href="https://github.com/${data.username}"><img src="./assets/images/github-brands.svg" height="40px" width="auto" alt="github icon"> </a>Visit my GitHub at <a href="https://github.com/${data.username}">github.com/${data.username}</a>\n
+  If you still have questions, please send me an email and I will respond as soon as I can:\n
+  <a href="mailto:${data.email}">${data.email}</a>\n
+`;
 }
+
+module.exports = generateMarkdown;
