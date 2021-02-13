c4367b49e6341fe10864f84ef45ae1dd010d2329
Liz Giddings <lgiddings03@gmail.com>
2021-02-12 19:02:47 -0600

    api.js file created.

--- docs/Develop/index.js
+++ docs/Develop/index.js
@@ -1,7 +1,50 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: "Add your Github username without @ symobol",
+        name: 'Username',
+        default: 'lgiddings03',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("valid Github username");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "Give your repository a name",
+        name: 'Repository',
+        default: 'readme-generatorReadmeGen',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("Must name your repo in order to get a badge");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "Breifly describe your project",
+        name: 'description',
+        default: 'Project Description',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("Description is required.");
+            }
+            return true;
+        }
+    },
+    
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
