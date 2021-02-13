4019cfbc33ecc616b20e54c206c8bd520b263112
Liz Giddings <lgiddings03@gmail.com>
2021-02-13 11:34:52 -0600

    adjusted Markdown function

--- docs/Develop/utils/generateMarkdown.js
+++ docs/Develop/utils/generateMarkdown.js
@@ -1,19 +1,25 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  await octokit.request('GET /licenses')
+  await licenseBadge();
+
+  async function licenseBadge() {
+    await octokit.request('GET /licenses');
+  }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  ![GitHub](https://img.shields.io/github/license/lgiddings03/ReadmeGen?style=for-the-badge)
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  await octokit.request('GET /repos/{owner}/{repo}/license', {
-    owner: 'octocat',
-    repo: 'hello-world'
+  await octokit.request('GET /repos/{lgiddings03}/{ReadmeGen}/license', {
+    owner: 'Liz Giddings',
+    repo: 'ReadmeGen'
   })
 }
 
@@ -25,5 +31,5 @@ function generateMarkdown(_markdown) {
     typographer: true
   });
 
-module.exports = generateMarkdown;
+  module.exports = generateMarkdown;
 }
